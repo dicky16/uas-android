9d87e84a18da2072dc7a161342e302d180671f80
Dikky Setiawan <setyawandicky88@gmail.com>
2020-11-24 21:01:10 +0700

    tambah detail

---
+++ uasandroid/app/src/main/java/com/slim/uas_android/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.slim.uas_android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.slim.uas_android.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
