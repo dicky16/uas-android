c733a870d600e71f7dc42563cc666ce690c3c28d
raihan <rbachtiar2425@gmail.com>
2020-12-09 21:50:12 +0700

    tambah 2 layout

--- uasandroid/app/src/main/java/com/slim/uas_android/ui/RegisterActivity.kt
+++ uasandroid/app/src/main/java/com/slim/uas_android/ui/RegisterActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.slim.uas_android.R
 
-class Pinjaman : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pinjaman)
+        setContentView(R.layout.activity_register)
     }
 }
